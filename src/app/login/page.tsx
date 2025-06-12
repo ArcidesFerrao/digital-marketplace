04d2f2230b355e8a51b6ec758171eab7a9f73944
ArcidesFerrao <cidesferrao@gmail.com>
2025-06-12 11:30:07 +0200

    login forgot pass word

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,25 +3,32 @@ import React from "react";
 
 export default function LoginPage() {
   return (
-    <main className="login-section flex flex-col justify-center gap-10 p-10">
-      <h2 className="text-4xl font-medium">Login</h2>
-      <section className="flex flex-col gap-5">
-        <input type="text" name="email" id="email" placeholder="Email" />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-        />
+    <main className="login-section flex flex-col justify-center gap-6 p-10">
+      <section className="flex flex-col gap-2">
+        <h2 className="text-4xl font-medium">Login</h2>
+        <div className="flex gap-1">
+          <p>I don&apos;t have an account yet. </p>
+          <Link href="/signup">Sign Up</Link>
+        </div>
       </section>
-      <section className="flex flex-col items-center gap-2">
+      <section className="flex flex-col gap-8">
+        <input type="text" name="email" id="email" placeholder="Email" />
+        <div className="flex flex-col gap-2">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <Link href="/">
+            <p>Forgot Password?</p>
+          </Link>
+        </div>
+      </section>
+      <section className="flex flex-col  gap-2">
         <button type="submit" className="w-full">
           Submit
         </button>
-        <div className="flex gap-1">
-          <p>I don't have an account yet. </p>
-          <Link href="/signup">Sign Up</Link>
-        </div>
       </section>
     </main>
   );
