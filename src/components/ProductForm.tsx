498ab1e53b3530479476c9b2edb27c817d636c38
ArcidesFerrao <cidesferrao@gmail.com>
2025-06-12 21:32:45 +0200

    product form radio category

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const ProductForm = () => {
   return (
     <form
-      className="flex flex-col justify-between min-w-80 gap-5 p-8"
+      className="flex flex-col justify-between min-w-96 gap-5 p-10"
       id="product"
     >
       <section>
@@ -11,28 +11,55 @@ export const ProductForm = () => {
       </section>
       <section className="flex flex-col gap-5">
         <input type="text" name="name" id="name" placeholder="Product Name" />
-        <div className="flex gap-2">
+        <div className="flex gap-2 items-center">
           <label htmlFor="number">Price:</label>
           <input type="number" name="price" id="price" />
         </div>
-        <fieldset className="flex flex-col">
-          <legend>Category:</legend>
+        <fieldset className="flex">
+          <legend>Category</legend>
           <span>
-            <input type="radio" name="category" value="ebook" /> E-book
+            <input
+              type="radio"
+              name="category"
+              value="ebook"
+              className="sr-only"
+            />{" "}
+            E-book
           </span>
           <span>
-            <input type="radio" name="category" value="template" /> Template
+            <input
+              type="radio"
+              name="category"
+              value="template"
+              className="sr-only"
+            />{" "}
+            Template
           </span>
           <span>
-            <input type="radio" name="category" value="pack" /> Music Pack
+            <input
+              type="radio"
+              name="category"
+              value="pack"
+              className="sr-only"
+            />{" "}
+            Music Pack
           </span>
           <span>
-            <input type="radio" name="category" value="stock" /> Stock
-            Photography
+            <input
+              type="radio"
+              name="category"
+              value="stock"
+              className="sr-only"
+            />{" "}
+            Stock Photography
           </span>
         </fieldset>
 
-        <textarea name="detail" id="detail" />
+        <textarea
+          name="detail"
+          id="detail"
+          placeholder="About the product..."
+        />
       </section>
       <button>Submit</button>
     </form>
