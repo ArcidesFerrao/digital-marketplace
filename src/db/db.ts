081e3880bbc31f70f7977c89b1d7a133bce8a420
ArcidesFerrao <cidesferrao@gmail.com>
2025-06-19 18:14:39 +0200

    prisma connected

--- src/db/db.ts
+++ src/db/db.ts
@@ -13,15 +13,3 @@ const db = globalThis.prismaGlobal ?? prismaClientSingleton()
 export default db 
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = db
-
-// const globalForPrisma = globalThis as unknown as {
-//     prismaGlobal: PrismaClient | undefined;
-// }
-
-
-// const db = globalForPrisma.prismaGlobal ?? new PrismaClient();
-
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prismaGlobal = db;
-
-// export default db;
