49e5e0b7eceeebda289703875d86f4cdaf0f69c4
ArcidesFerrao <cidesferrao@gmail.com>
2025-06-22 16:30:22 +0200

    message page render

--- src/app/admin/message/[id]/page.tsx
+++ src/app/admin/message/[id]/page.tsx
@@ -1,5 +1,31 @@
+import { AdminHeader } from "@/components/AdminHeader";
+import db from "@/db/db";
 import React from "react";
 
-export default function MessagePage() {
-  return <div>MessagePage</div>;
+export default async function MessagePage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const message = await db.message.findUnique({
+    where: { id },
+  });
+  return (
+    <main className="admin-section p-20 flex flex-col gap-10">
+      <AdminHeader />
+
+      <section className="message flex flex-col gap-5 p-5">
+        <h2>Message from: {`${message?.name}  ${message?.lastName}`}</h2>
+        <p>{new Date(message?.createdAt).toLocaleDateString()}</p>
+        <div className="content-msg">
+          <p>{message?.content}</p>
+        </div>
+      </section>
+      <section className="reply-msg">
+        <button>Reply</button>
+      </section>
+    </main>
+  );
 }
