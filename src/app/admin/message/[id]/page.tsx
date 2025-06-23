8c98c1574f167308d8d12683d4f0a70a22645c14
ArcidesFerrao <cidesferrao@gmail.com>
2025-06-23 17:17:09 +0200

    message style

--- src/app/admin/message/[id]/page.tsx
+++ src/app/admin/message/[id]/page.tsx
@@ -12,14 +12,27 @@ export default async function MessagePage({
   const message = await db.message.findUnique({
     where: { id },
   });
+
+  if (!message) {
+    return (
+      <main className="admin-section p-20 flex flex-col gap-10">
+        <AdminHeader />
+        <div className="content-msg  p-10">
+          <p>Mensagem nao encontrada</p>
+        </div>
+      </main>
+    );
+  }
   return (
     <main className="admin-section p-20 flex flex-col gap-10">
       <AdminHeader />
 
-      <section className="message flex flex-col gap-5 p-5">
-        <h2>Message from: {`${message?.name}  ${message?.lastName}`}</h2>
-        <p>{new Date(message?.createdAt).toLocaleDateString()}</p>
-        <div className="content-msg">
+      <section className="message flex flex-col">
+        <div className="header-msg flex justify-between  p-5">
+          <h2>Message from: {`${message?.name}  ${message?.lastName}`}</h2>
+          <p>{new Date(message.createdAt).toLocaleDateString()}</p>
+        </div>
+        <div className="content-msg  p-10">
           <p>{message?.content}</p>
         </div>
       </section>
