5b2e5992affe997f72a487892a945a15c72939d2
ArcidesFerrao <cidesferrao@gmail.com>
2025-06-30 20:17:09 +0200

    dashboard data from db

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,17 @@
 import { AdminHeader } from "@/components/AdminHeader";
+import { getApprovedProducts, getProducts } from "@/lib/getProducts";
 import React from "react";
 
-export default function AdminPage() {
+export default async function AdminPage() {
+  const products = await getApprovedProducts();
+  const requests = await getProducts();
   return (
     <main className="admin-section p-20 flex flex-col gap-10">
       <AdminHeader />
       <section className="overview flex justify-around">
         <div className="dash-card">
           <h3>Produtos</h3>
-          <h1>12</h1>
+          <h1>{products.length}</h1>
         </div>
         <div className="dash-card">
           <h3>Total de Vendas</h3>
@@ -16,7 +19,7 @@ export default function AdminPage() {
         </div>
         <div className="dash-card">
           <h3>Pedidos</h3>
-          <h1>156</h1>
+          <h1>{requests.length}</h1>
         </div>
       </section>
     </main>
