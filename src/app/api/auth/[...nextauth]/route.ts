6e23cf2d870f544d1bd778774020fa39ed9641ab
ArcidesFerrao <cidesferrao@gmail.com>
2025-07-09 21:15:26 +0200

    nextauth with google provider set

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,100 @@
-import { authOptions } from "@/lib/auth";
-import NextAuth from "next-auth/next";
 
-const handler = NextAuth(authOptions);
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth/next";
+import Google from "next-auth/providers/google";
+
+const db = new PrismaClient;
+
+const handler = NextAuth({
+    providers: [
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID!,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+        }),
+    ],
+    adapter: PrismaAdapter(db),
+    session: {
+        strategy: "database",
+    },
+    callbacks: {
+        async session({ session, token, user}) {
+            if (user) {
+                session.user = {
+                ...session.user,
+                // id: user.id,
+                name: user.name,
+                email: user.email,
+                image: user.image,
+                }
+            } else if (token) {
+                session.user = {
+                ...session.user,
+                // id: token.sub as string,
+                name: token.name,
+                email: token.email
+                };
+            }
+            return session
+        },
+        async signIn({ user, account}) {
+            if (account?.provider === "google" ) {
+                        const existingAccount = await db.account.findUnique({
+                            where: {
+                                provider_providerAccountId: {
+                                    provider: account.provider,
+                                    providerAccountId: account.providerAccountId,
+                                }
+                            },
+                        });
+                        if (!existingAccount) {
+                            const existingUser = await db.user.findUnique({
+                                where: {
+                                    email: user.email || "",
+                                }
+                            })
+
+                            if (existingUser) {
+                                await db.account.create({
+                                    data: {
+                                        userId: existingUser.id,
+                                        provider: account.provider,
+                                        type: account.provider,
+                                        providerAccountId: account.providerAccountId,
+
+                                        access_token: account.access_token ?? null,
+                                    },
+                                });
+                            } else {
+                                if (user) {
+                                    const newUser = await db.user.create({
+                                        data: {
+                                            email: user.email!,
+                                            name: user.name!,
+                                            image: user.image,
+                                            // password: user.password,
+                                        }
+                                    });
+
+                                    await db.account.create({
+                                        data: {
+                                            userId: newUser.id,
+                                            provider: account.provider,
+                                            providerAccountId: account.providerAccountId,
+                                            access_token: account.access_token,
+                                            type: account.type,
+                                        }
+                                    })
+                                }
+                            }
+                        } 
+                    }
+                    return true;
+        },
+
+    },
+    }
+);
 
 export const GET = handler;
 export const POST = handler;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,19 +1,98 @@
 import db from "@/db/db";
-import NextAuth from "next-auth/next";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import Google from "next-auth/providers/google";
-import type { SessionStrategy } from "next-auth";
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import { AuthConfig } from "@auth/core"
+import Google from "next-auth/providers/google"
+
 
 
 export const authOptions = {
   adapter: PrismaAdapter(db),
+  
   providers: [
     Google({
-    clientId: process.env.AUTH_GOOGLE_ID!,
-    clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-  })],
-  session: { strategy: "jwt" as SessionStrategy },
-  
-};
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user}) {
+      if (user) {
+        session.user = {
+          ...session.user,
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          image: user.image,
+        }
+      } else if (token) {
+        session.user = {
+          ...session.user,
+          id: token.sub as string,
+          name: token.name,
+          email: token.email
+        };
+      }
+      return session
+    }
+  },
+  async signIn({ user, account}) {
+    if (account?.provider === "google" ) {
+                const existingAccount = await db.account.findUnique({
+                    where: {
+                        provider_providerAccountId: {
+                            provider: account.provider,
+                            providerAccountId: account.providerAccountId,
+                        }
+                    },
+                });
+                if (!existingAccount) {
+                    const existingUser = await db.user.findUnique({
+                        where: {
+                            email: user.email || "",
+                        }
+                    })
 
-export default NextAuth(authOptions)
+                    if (existingUser) {
+                        await db.account.create({
+                            data: {
+                                userId: existingUser.id,
+                                provider: account.provider,
+                                type: account.provider,
+                                providerAccountId: account.providerAccountId,
+
+                                access_token: account.access_token ?? null,
+                            },
+                        });
+                    } else {
+                        if (user) {
+                            const newUser = await db.user.create({
+                                data: {
+                                    email: user.email,
+                                    name: user.name,
+                                    image: user.image,
+                                    password: user.password,
+                                }
+                            });
+
+                            await db.account.create({
+                                data: {
+                                    userId: newUser.id,
+                                    provider: account.provider,
+                                    providerAccountId: account.providerAccountId,
+                                    access_token: account.access_token,
+                                    type: account.type,
+                                }
+                            })
+                        }
+                    }
+                } 
+            }
+            return true;
+  },
+  session: {
+    strategy: "database",
+    maxAge: 24 * 60 * 60,
+  }
+  
+} 
