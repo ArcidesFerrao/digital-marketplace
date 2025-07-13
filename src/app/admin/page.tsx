2b3c746089528e6debdb46f7fb34ffa2bad31834
ArcidesFerrao <cidesferrao@gmail.com>
2025-07-13 21:49:02 +0200

    comission admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -7,13 +7,20 @@ export default async function AdminPage() {
   const products = await getApprovedProducts();
   const requests = await getProducts();
   const totalAmount = await getTotalAmount();
+  let amount = 0;
+  if (!totalAmount) {
+    amount = 0;
+  } else {
+    amount = totalAmount._sum.amount || 0;
+  }
   return (
     <main className="admin-section p-20 flex flex-col gap-10">
       <AdminHeader />
-      <section className="overview flex justify-around">
+      <section className="overview flex  justify-around">
         <div className="dash-card">
           <h3>Total de Vendas</h3>
           <h1>MZN {totalAmount._sum.amount}.00</h1>
+          <p>Comission (10%): {`${amount === 0 ? 0 : amount * 0.1}.00`} </p>
         </div>
         <div className="dash-card">
           <h3>Produtos</h3>
