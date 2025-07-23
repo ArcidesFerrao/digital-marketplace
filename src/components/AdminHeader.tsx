8f4668b59c054aa4838ff9a77e2649e9f47e5949
ArcidesFerrao <cidesferrao@gmail.com>
2025-07-23 12:19:51 +0200

    admin dash menu

--- src/components/AdminHeader.tsx
+++ src/components/AdminHeader.tsx
@@ -1,16 +1,50 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export const AdminHeader = () => {
+  const menuRef = useRef<HTMLDivElement>(null);
+  const [menu, setMenu] = useState(false);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setMenu(false);
+      }
+    }
+
+    if (menu) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menu]);
+
   return (
-    <section className="dash-header flex justify-between">
+    <section className="dash-header flex items-center justify-between">
       <Link href="/admin">
         <h2>Dashboard</h2>
       </Link>
-      <div className="flex gap-5">
+      <div className="dash-menu-admin flex gap-5">
         <Link href="/admin/request">Requests</Link>
         <Link href="/admin/report">Report</Link>
         <Link href="/admin/message">Messages</Link>
+      </div>
+      <div className="dash-nav-admin" ref={menuRef}>
+        {!menu ? (
+          <button className="flex " onClick={() => setMenu((prev) => !prev)}>
+            <span className="ion--menu-outline"></span>
+          </button>
+        ) : (
+          <div className="flex gap-5">
+            <Link href="/admin/request">Requests</Link>
+            <Link href="/admin/report">Report</Link>
+            <Link href="/admin/message">Messages</Link>
+          </div>
+        )}
       </div>
     </section>
   );
